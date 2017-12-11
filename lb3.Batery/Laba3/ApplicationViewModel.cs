61d84f78a2985e0286b3296fd713f0b484bad368
Artem Bobko <artembobko@gmail.com>
2017-12-11 16:45:05 +0300

    Add lb3

---
+++ lb3.Batery/Laba3/ApplicationViewModel.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+using System.Text.RegularExpressions;
+
+namespace Laba3
+{
+    class ApplicationViewModel : INotifyPropertyChanged
+    {  
+
+        #region XAML updater 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        #endregion
+
+        private Timer timer;
+        private float timeWithPower;
+        private float timeNotPower;
+        private float InitialDCTime = -1;
+        private float InitialACTime = -1;
+        private readonly BatteryManager batteryManager = new BatteryManager();
+        
+        public string LineStatus
+        {
+            get
+            {
+                if (batteryManager.PowerStatus.ACLineStatus == 1)
+                    return "AC";
+                return "DC";
+            }
+
+        }
+
+        public string BatteryPersent
+        {
+            get
+            {
+                if (batteryManager.PowerStatus.BatteryLifePercent > 100)
+                {
+                    return "-";
+                }
+                else
+                {
+                    return batteryManager.PowerStatus.BatteryLifePercent.ToString();
+                }
+            }
+
+        }
+        
+        public double BatteryProgress
+        {
+            get
+            {
+                if (batteryManager.PowerStatus.BatteryLifePercent > 100)
+                {
+                    return 100.0;
+                }
+                return batteryManager.PowerStatus.BatteryLifePercent;
+            }
+            set
+            {
+                //OnPropertyChanged("BatteryProgress");
+            }
+        }
+        
+
+        public string BatteryLifeTime
+        {
+            get {
+                if(batteryManager.PowerStatus.BatteryLifeTime != -1)
+                {
+                    return (batteryManager.PowerStatus.BatteryLifeTime/60).ToString() + " мин";
+                }
+                return "-";
+            }
+            
+        }
+
+        public float DisplayTimeAC
+        {
+            get
+            {
+                return timeWithPower;
+            }
+            set
+            {                
+               timeWithPower = value;
+                if (timeWithPower!=0)
+                {
+                    batteryManager.ChangeScreenTime(ACDC_STATUS.AC ,timeWithPower);
+                }
+                OnPropertyChanged("TimeWithPower");
+            }
+        }
+
+        public float DisplayTimeDC
+        {
+            get
+            {
+                return timeNotPower;
+            }
+
+            set
+            {
+                timeNotPower = value;
+                if ( timeNotPower != 0)
+                {
+                    batteryManager.ChangeScreenTime(ACDC_STATUS.DC, timeNotPower);
+
+                }
+                OnPropertyChanged("TimeNotPower");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            ManageBatteryLevel();
+
+            batteryManager.GetScreenTime(ref timeWithPower, ref timeNotPower);
+            InitialDCTime = timeWithPower;
+            InitialACTime = timeNotPower;
+            MainWindow.OnWindowClosed += OnWindowClosed;
+        }
+
+        private void OnWindowClosed()
+        {
+            batteryManager.ChangeScreenTime(ACDC_STATUS.AC, InitialACTime);
+            batteryManager.ChangeScreenTime(ACDC_STATUS.DC, InitialDCTime);
+        }
+
+        private void OnBatteryLevelChange(Object stateInfo)
+        {
+            batteryManager.UpdateInfo();
+
+            OnPropertyChanged("LineStatus");
+            OnPropertyChanged("BatteryLifeTime");
+            OnPropertyChanged("BatteryPersent");
+            OnPropertyChanged("BatteryProgress");
+        }
+
+        private void ManageBatteryLevel()
+        {
+            TimeSpan dueTime = new TimeSpan(0, 0, 0);
+            TimeSpan period = new TimeSpan(0, 0, 1);
+
+            TimerCallback timeCB = new TimerCallback(OnBatteryLevelChange);
+            timer = new Timer(timeCB, null, dueTime, period);
+        }
+
+    }
+}
+
---
+++ lb3.Batery/Laba3/BatteryManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace Laba3
+{
+    class BatteryManager
+    {
+   
+        [DllImport("kernel32")]
+        public static extern int GetSystemPowerStatus(ref POWER_STATUS lpSystemPowerStatus);
+
+        private POWER_STATUS powerStatus;
+
+        public POWER_STATUS PowerStatus
+        {
+            get
+            {
+                return powerStatus;
+            }
+            private set
+            {
+                powerStatus = value;
+            }
+        }
+
+        
+        public BatteryManager()
+        {
+            PowerStatus = Init();
+        }
+
+
+        public void UpdateInfo()
+        {
+            PowerStatus = Init();
+            GetSystemPowerStatus(ref powerStatus);
+        }
+
+        private POWER_STATUS Init()
+        {
+            POWER_STATUS tempSPS;
+            
+            tempSPS.ACLineStatus = 1;
+            tempSPS.BatteryFlag = 1;
+            tempSPS.BatteryLifePercent = 1;
+            tempSPS.SystemStatusFlag = 1;
+            tempSPS.BatteryLifeTime = 1;
+            tempSPS.BatteryFullLifeTime = 1;
+            return tempSPS;
+        }
+
+        public void ChangeScreenTime(ACDC_STATUS status, float value)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            switch (status)
+            {
+                case ACDC_STATUS.AC:
+                    process.StartInfo.Arguments = "/c powercfg /x -monitor-timeout-ac " + value;
+                    break;
+
+                case ACDC_STATUS.DC:
+                    process.StartInfo.Arguments = "/c powercfg /x -monitor-timeout-dc " + value;
+                    break;
+            }
+                        
+            process.Start();
+        }
+        
+
+        private string ParseCMD(string arguments)
+        {
+            var process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            
+            return process.StandardOutput.ReadToEnd();
+        }
+
+        public void GetScreenTime(ref float timeWithPower, ref float timeNotPower)
+        {
+            var powercfg = ParseCMD("/c powercfg /q");
+
+            var information = powercfg.Split(new[] { "GUID" }, StringSplitOptions.None).ToList()
+                .Find(i => i.Contains("VIDEOIDLE"))
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            timeWithPower = Convert.ToInt32(information[5].Split(new[] { ' ' }).Last().Split(new[] { 'x' }).Last(), 16) / 60;
+            timeNotPower = Convert.ToInt32(information[6].Split(new[] { ' ' }).Last().Split(new[] { 'x' }).Last(), 16) / 60;
+        }
+
+    }
+}
